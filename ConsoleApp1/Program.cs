25048a58a4337b2cbffa44f368b0ad2d78429d88
Ivan Srsen <ivan.srsen@kandasoft.com>
2020-09-23 19:37:48 +0200

    Changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClassLibrary2;
 
 namespace ConsoleApp1
 {
@@ -10,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Class1.Print();
         }
     }
 }
