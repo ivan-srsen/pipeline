fc36e7763ef110dfb073d4d70163b8608c4fc80a
Ivan Srsen <ivan.srsen@kandasoft.com>
2020-09-26 14:57:42 +0200

    Fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var test = new Class2{Test = "sadsdada"};
+            var test1 = new Class2 { Test = "sadsdada" };
             JsonConvert.SerializeObject(test);
             Class1.Print();
         }
